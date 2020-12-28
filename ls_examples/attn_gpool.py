b27a2d5286ceaf8155b88bd712c1aec331868d86
Guangxiang Zhao <zhaoguangxiang@pku.edu.cn>
2020-12-28 14:35:09 +0800

    base code for attention based graph pooling

---
+++ ls_examples/attn_gpool.py
@@ -0,0 +1,133 @@
+from typing import Union, Optional, Callable
+
+import torch
+from torch_geometric.nn import GraphConv
+from torch_geometric.nn.pool.topk_pool import topk, filter_adj
+from torch_geometric.utils import softmax
+
+
+# base code for attention graph pooling, we sum attention scores Attention ij for each j.
+# two types:
+# attn_type in ['all_attn','neighbor_attn']
+# we want to add the degree information to adjust the scores.
+# but learning method might be hard.
+# 1. all_attn: without considering A
+# *****1.1 sum Attention ij for each j
+# **1.2 c1*Aj +c2*Dj consider degree  c1 and c2 are n_dim parameter
+# *1.2.1 tanh or bias
+#
+# 2. neighbour_attn: attn only on neighbour
+# *****2.1 mean Attention ij for each j * D
+# ****2.2 mean Attention ij for each j
+# ***2.3 mean Attention ij for each j * D*learnable rate for each D
+# ***2.4 mean Attention ij for each j * learnable discounf for all Degree
+# 2.5 input degree and learn the tradeoff implicitly
+class AttnPooling(torch.nn.Module):
+    r"""The attention pooling operator from the
+
+    if :obj:`min_score` :math:`\tilde{\alpha}` is :obj:`None`:
+
+        .. math::
+            \mathbf{y} &= \textrm{GNN}(\mathbf{X}, \mathbf{A})
+
+            \mathbf{i} &= \mathrm{top}_k(\mathbf{y})
+
+            \mathbf{X}^{\prime} &= (\mathbf{X} \odot
+            \mathrm{tanh}(\mathbf{y}))_{\mathbf{i}}
+
+            \mathbf{A}^{\prime} &= \mathbf{A}_{\mathbf{i},\mathbf{i}}
+
+    if :obj:`min_score` :math:`\tilde{\alpha}` is a value in [0, 1]:
+
+        .. math::
+            \mathbf{y} &= \mathrm{softmax}(\textrm{GNN}(\mathbf{X},\mathbf{A}))
+
+            \mathbf{i} &= \mathbf{y}_i > \tilde{\alpha}
+
+            \mathbf{X}^{\prime} &= (\mathbf{X} \odot \mathbf{y})_{\mathbf{i}}
+
+            \mathbf{A}^{\prime} &= \mathbf{A}_{\mathbf{i},\mathbf{i}},
+
+    where nodes are dropped based on a learnable projection score
+    :math:`\mathbf{p}`.
+    Projections scores are learned based on a graph neural network layer.
+
+    Args:
+        in_channels (int): Size of each input sample.
+        ratio (float or int): Graph pooling ratio, which is used to compute
+            :math:`k = \lceil \mathrm{ratio} \cdot N \rceil`, or the value
+            of :math:`k` itself, depending on whether the type of :obj:`ratio`
+            is :obj:`float` or :obj:`int`.
+            This value is ignored if :obj:`min_score` is not :obj:`None`.
+            (default: :obj:`0.5`)
+        GNN (torch.nn.Module, optional): A graph neural network layer for
+            calculating projection scores (one of
+            :class:`torch_geometric.nn.conv.GraphConv`,
+            :class:`torch_geometric.nn.conv.GCNConv`,
+            :class:`torch_geometric.nn.conv.GATConv` or
+            :class:`torch_geometric.nn.conv.SAGEConv`). (default:
+            :class:`torch_geometric.nn.conv.GraphConv`)
+        min_score (float, optional): Minimal node score :math:`\tilde{\alpha}`
+            which is used to compute indices of pooled nodes
+            :math:`\mathbf{i} = \mathbf{y}_i > \tilde{\alpha}`.
+            When this value is not :obj:`None`, the :obj:`ratio` argument is
+            ignored. (default: :obj:`None`)
+        multiplier (float, optional): Coefficient by which features gets
+            multiplied after pooling. This can be useful for large graphs and
+            when :obj:`min_score` is used. (default: :obj:`1`)
+        nonlinearity (torch.nn.functional, optional): The nonlinearity to use.
+            (default: :obj:`torch.tanh`)
+        **kwargs (optional): Additional parameters for initializing the graph
+            neural network layer.
+    """
+    def __init__(self, in_channels: int, ratio: Union[float, int] = 0.5,
+                 GNN: Callable = GraphConv, min_score: Optional[float] = None,
+                 multiplier: float = 1.0, nonlinearity: Callable = torch.tanh,
+                 attn_type: str = 'all_attn',
+                 **kwargs):
+        super(AttnPooling, self).__init__()
+
+        self.in_channels = in_channels
+        self.ratio = ratio
+        self.gnn = GNN(in_channels, 1, **kwargs)
+        self.min_score = min_score
+        self.multiplier = multiplier
+        self.nonlinearity = nonlinearity
+
+        self.reset_parameters()
+        self.attn_type = attn_type
+
+    def reset_parameters(self):
+        self.gnn.reset_parameters()
+
+    def forward(self, x, edge_index, edge_attr=None, batch=None, attn=None):
+        """"""
+        if batch is None:
+            batch = edge_index.new_zeros(x.size(0))
+
+        attn = x if attn is None else attn
+        attn = attn.unsqueeze(-1) if attn.dim() == 1 else attn
+        score = self.gnn(attn, edge_index).view(-1)
+
+        if self.min_score is None:
+            score = self.nonlinearity(score)
+        else:
+            score = softmax(score, batch)
+
+        perm = topk(score, self.ratio, batch, self.min_score)
+        x = x[perm] * score[perm].view(-1, 1)
+        x = self.multiplier * x if self.multiplier != 1 else x
+
+        batch = batch[perm]
+        edge_index, edge_attr = filter_adj(edge_index, edge_attr, perm,
+                                           num_nodes=score.size(0))
+
+        return x, edge_index, edge_attr, batch, perm, score[perm]
+
+    def __repr__(self):
+        return '{}({}, {}, {}={}, multiplier={})'.format(
+            self.__class__.__name__, self.gnn.__class__.__name__,
+            self.in_channels,
+            'ratio' if self.min_score is None else 'min_score',
+            self.ratio if self.min_score is None else self.min_score,
+            self.multiplier)
